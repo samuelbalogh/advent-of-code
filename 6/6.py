cb6f4ba3a6620965ac57f01e0e3520af6c8dd487
MaxMontoro <samubalogh@gmail.com>
2016-12-21 16:50:06 +0100

    add 6

---
+++ 6/6.py
@@ -0,0 +1,37 @@
+from string import ascii_lowercase
+
+""" First part """
+def find_consensus_string(path):
+    with open(path, 'r') as data:
+        profile = {i : {j : 0 for j in ascii_lowercase} for i in range(8)}
+        while True:
+            try:
+                line = next(data)
+                for index in range(len(line.strip())):
+                        profile[index][line[index]] += 1
+            except StopIteration:
+                break
+    result =''.join([sorted(profile[index], key=profile[index].get, reverse=True)[0] for index in profile])
+    print(result)
+    return  result
+
+find_consensus_string('input')
+
+
+""" Second part """
+
+def find_consensus_string(path):
+    with open(path, 'r') as data:
+        profile = {i : {j : 0 for j in ascii_lowercase} for i in range(8)}
+        while True:
+            try:
+                line = next(data)
+                for index in range(len(line.strip())):
+                        profile[index][line[index]] += 1
+            except StopIteration:
+                break
+    result =''.join([sorted(profile[index], key=profile[index].get)[0] for index in profile])
+    print(result)
+    return  result
+
+find_consensus_string('input')

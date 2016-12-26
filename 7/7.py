ce51e398081ef5f88c01a5a51be99c9549ce8ef8
MaxMontoro <samubalogh@gmail.com>
2016-12-26 16:00:53 +0100

    add part 2 of 7

--- 7/7.py
+++ 7/7.py
@@ -1,5 +1,7 @@
 from itertools import count
 
+''' First part '''
+
 def is_pal(s):
     ''' Returns True if s is palindrome, False otherwise '''
     reverse = s[::-1]
@@ -40,4 +42,38 @@ def transport_layer_snooping(path):
     print(contains_tls)
     return contains_tls
 
-transport_layer_snooping('input')
+''' Second part '''
+
+def contains_palindrom_of_three_chars(s):
+    ''' Return True if s contains a palindrome of length four, composed of two different characters '''
+    palindromes = []
+    for i in range(len(s)-2):
+        if is_pal(s[i:i+3]) and len(set(s[i:i+3])) != 1:
+            palindromes.append(s[i:i+3])
+    return palindromes
+
+def super_secret_listening(path):
+    ''' Main function '''
+    with open(path, 'r') as data:
+        contains_ssl = 0
+        for _ in count():
+            try:
+                line = next(data)
+                slices = line.strip().replace('[', '#').replace(']', '#').split('#')
+                between_square_brackets = (slice for slice in slices if slices.index(slice) % 2 == 1)
+                outside_square_brackets = (slice for slice in slices if slices.index(slice) % 2 == 0)
+                inner_palindromes = [elem for sublist in [contains_palindrom_of_three_chars(slice) for slice in between_square_brackets] for elem in sublist]
+                outer_palindromes = [elem for sublist in [contains_palindrom_of_three_chars(slice) for slice in outside_square_brackets] for elem in sublist]
+                for inner in inner_palindromes:
+                    for outer in outer_palindromes:
+                        if set(inner) == set(outer) and inner != outer:
+                            contains_ssl += 1
+                            raise ContinueWithNextLine
+            except ContinueWithNextLine:
+                continue
+            except StopIteration:
+                break
+    print(contains_ssl)
+    return contains_ssl
+
+super_secret_listening('input')

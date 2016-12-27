b76bb720a20f31d8cf54355b15159a2ba51a72a5
MaxMontoro <samubalogh@gmail.com>
2016-12-27 18:39:20 +0100

    add part 2 (kinda didnt notice it)

--- 1/1.py
+++ 1/1.py
@@ -1,3 +1,5 @@
+""" Part one"""
+
 directions = {
               'N':
                   {'L': 'W',
@@ -17,6 +19,7 @@ directions = {
                    },
                   }
 
+"""
 progress = {'N': 0, 'S': 0, 'E': 0, 'W': 0}
 
 with open('input', 'r') as data:
@@ -29,4 +32,32 @@ with open('input', 'r') as data:
         progress[facing] += int(block.strip('RL '))
 
 total_distance = abs(progress['N'] - progress['S']) + abs(progress['E'] - progress['W'])
-print abs(total_distance)
+print(abs(total_distance))
+"""
+
+""" Part two """
+
+progress = {'N': 0, 'S': 0, 'E': 0, 'W': 0}
+positions = {}
+
+def back_to_square_one():
+    with open('input', 'r') as data:
+        facing = 'N'
+        line = data.readline()
+        progress[directions[facing][line.split(',')[0][0]]] += int(line.split(',')[0][1])
+        facing = directions[facing][line.split(',')[0][0]]
+        step = 0
+        positions[step] = progress['N'] - progress['S'], progress['E'] - progress['W']
+        for block in line.split(',')[1:]:
+            facing = directions[facing][block.strip()[0]]
+            for i in range(int(block.strip('RL '))):
+                step += 1
+                progress[facing] += 1
+                if (progress['N'] - progress['S'], progress['E'] - progress['W']) in positions.values():
+                    total_distance = abs(progress['N'] - progress['S']) + abs(progress['E'] - progress['W'])
+                    print(total_distance)
+                    return total_distance
+                positions[step] = progress['N'] - progress['S'], progress['E'] - progress['W']
+        print(positions)
+
+back_to_square_one()

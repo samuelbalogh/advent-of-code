ec3c1d58165934174fbb237348e0c2553159c6da
MaxMontoro <samubalogh@gmail.com>
2017-12-08 13:51:25 +0100

    add 02/01

---
+++ 2017/02/01.py
@@ -0,0 +1,17 @@
+
+
+def get_checksum():
+    with open('input', 'r') as input_data:
+        checksum = 0
+        for line in input_data:
+            nums = [int(item) for item in line.split()]
+            diff = max(nums) - min(nums)
+            checksum += diff
+    return checksum
+
+
+def get_checksum_with_comprehension():
+    with open('input', 'r') as input_data:
+        return sum((max([int(item) for item in line.split()]) - min([int(item) for item in line.split()])) for line in input_data)
+
+print(get_checksum())

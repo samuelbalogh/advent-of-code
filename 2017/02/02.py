fccdc422a7fd83ea65f22dba23a8c986d55d57b8
MaxMontoro <samubalogh@gmail.com>
2017-12-08 14:10:50 +0100

    add 2d part of day2

---
+++ 2017/02/02.py
@@ -0,0 +1,19 @@
+
+
+MULTIPLICATION_TABLE = {}
+
+
+def get_checksum():
+    with open('input', 'r') as input_data:
+        checksum = 0
+        for line in input_data:
+            nums = sorted([int(item) for item in line.split()])
+            for index, num in enumerate(nums):
+                dividend = [larger_num for larger_num in nums[index+1:] if larger_num % num == 0]
+                if dividend:
+                    checksum += dividend.pop() // num
+                    continue
+    return checksum
+
+
+print(get_checksum())

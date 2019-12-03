7025f940f277080f4a9592855c39f6b979497c7a
Samuel Balogh <samuel@ometria.com>
2019-12-03 16:38:44 +0000

    add 2b

--- 2019/02/1.py
+++ 2019/02/1.py
@@ -19,7 +19,6 @@ intcode = [1, 0, 0, 0, 99]
 
 
 def process_intcode(intcode):
-    result = intcode
     for index in range(0, len(intcode), 4):
         opcode = intcode[index]
         inp1, inp2 = intcode[intcode[index + 1]], intcode[intcode[index + 2]]
@@ -42,9 +41,25 @@ def main1():
     chars[1] = 12
     chars[2] = 2
     intcode = process_intcode(chars)
+    return intcode
 
 
 ## PART 2
+
+def main2():
+    intcode = []
+    for line in read_input("02/input.txt"):
+        intcode.extend([int(i) for i in read_chars(line)])
+
+    for i in range(0, 100):
+        for j in range(0, 100):
+            copy = intcode.copy()
+            copy[1] = i
+            copy[2] = j
+            res = process_intcode(copy)
+            if res[0] == 19690720:
+                print(f"{i}, {j} are the winners!")
+                break
 
 
 if __name__ == "__main__":

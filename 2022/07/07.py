bdd9fcef92e8946f8ef1692cc8ffe8f4bddd93de
Samuel Balogh <samubalogh@gmail.com>
2022-12-09 12:48:46 +0100

    minor simplifications for 07

--- 2022/07/07.py
+++ 2022/07/07.py
@@ -13,40 +13,30 @@ def read_input_file(path: Text = 'test_input.txt') -> Generator:
 
 def explore_file_system():
     file_system = defaultdict(dict)
-
-    listing = False
     path = []
-
     for line in read_input_file():
-        if listing:
-            if line.startswith('dir'):
-                directory = line.split('dir')[1].strip()
-                subtree = file_system
-                for step in path:
-                    subtree = subtree[step]
-                subtree[directory] = defaultdict(dict)
+        subtree = file_system
+        if line.startswith('dir'):
+            directory = line.split('dir')[1].strip()
+            for step in path:
+                subtree = subtree[step]
+            subtree[directory] = defaultdict(dict)
 
-            elif (filesize := line.split(' ')[0]).isdigit():
-                subtree = file_system
-                for step in path[:-1]:
-                    subtree = subtree[step]
-                if 'files' in subtree[path[-1]]:
-                    subtree[path[-1]]['files'] += int(filesize)
-                else:
-                    subtree[path[-1]]['files'] = int(filesize)
+        elif (filesize := line.split(' ')[0]).isdigit():
+            for step in path[:-1]:
+                subtree = subtree[step]
+            if 'files' in subtree[path[-1]]:
+                subtree[path[-1]]['files'] += int(filesize)
+            else:
+                subtree[path[-1]]['files'] = int(filesize)
 
         if line.startswith('$'):
             if '$ cd ' in line:
-                listing = False
                 target = line.split('$ cd ')[1]
                 if target == '..':
                     path.pop()
                 else:
                     path.append(target.strip())
-
-            elif '$ ls' in line:
-                listing = True
-
     return file_system
 
 
@@ -82,17 +72,13 @@ def main_part_two():
     space_required = 30000000
     total_space_available = 70000000
 
-    file_system = explore_file_system()
-    tree = file_system
+    tree = explore_file_system()
 
-    total_size_on_disk = get_total_size_of_tree(file_system)
-
+    total_size_on_disk = get_total_size_of_tree(tree)
     unused_space = total_space_available - total_size_on_disk
-
     required_purge_size = space_required - unused_space
 
     stack = deque([ ('/', tree) ])
-
     smallest = total_size_on_disk
     while stack:
         node, tree = stack.popleft()
